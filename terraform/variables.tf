2d7bce02814ebc6b76cef327e76c38093b939e1f
AndyA14 <andyrey2000@hotmail.com>
2025-12-15 22:13:54 -0500

    Format terraform files

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -23,11 +23,11 @@ variable "desired_capacity" {
 }
 
 variable "min_size" {
-  type        = number
-  default     = 4
+  type    = number
+  default = 4
 }
 
 variable "max_size" {
-  type        = number
-  default     = 6
+  type    = number
+  default = 6
 }
